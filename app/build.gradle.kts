9b56fbaaa26c97033c02e61a4bd18da2d531e3e0
vincenzo722 <74825997+vincenzo722@users.noreply.github.com>
2026-09-16 19:11:49 +0200

    Versione 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ android {
         applicationId = "it.violante.radarattivita"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
 }
